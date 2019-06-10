2dba964f4557a70db423a4a8720030f4f57285e0
Andrzej Jarzebowski <jarzebowski.andrzej@gmail.com>
2019-06-10 17:54:08 +0200

    GO day #0 solution. Print line and reading stdin

---
+++ Day 0/solution.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	// https://golang.org/pkg/bufio/
+	"bufio"
+	// https://golang.org/pkg/fmt/
+	"fmt"
+	// https://golang.org/pkg/os/
+	"os"
+)
+
+func main() {
+	// Printing variables from code
+	var helloWorld string = "Hello World!"
+	shorthandHelloWorld := "Shorthand Hello World!"
+
+	fmt.Println(helloWorld, "\n", shorthandHelloWorld)
+
+	// Printing variables from stdin
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Print("Enter text: ")
+	text, _ := reader.ReadString('\n')
+	fmt.Println(text)
+}
