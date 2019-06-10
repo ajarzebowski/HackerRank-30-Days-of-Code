f1798388d8a66831d3609274e20b1eeef85ad32f
Andrzej Jarzebowski <jarzebowski.andrzej@gmail.com>
2019-06-10 14:23:52 +0200

    Removed old solutions

--- Day 5/solution.py
+++
@@ -1,9 +0,0 @@
-#!/bin/python
-
-import sys
-
-n = int(raw_input().strip())
-
-for i in range(1, 11):
-    a = str(n) + ' x ' + str(i) + ' = ' + str(n*i)
-    sys.stdout.write (a + '\n')
--- Day 9/solution.py
+++
@@ -1,9 +0,0 @@
-def factorial(n):
-    if n == 1:
-        return 1
-    
-    return (n * factorial(n-1))
-
-n = int(raw_input())
-
-print (factorial(n))
